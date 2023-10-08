fd0bd86f3aded5aafc5af5d43a8025a114398c3b
shubham <shubhamrupapara993@gmail.com>
2023-10-08 22:21:21 +0530

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
 
         <div className="flexColStart f-right">
           <span className="primaryText">Information</span>
-          <span className="secondaryText">145, FL 5467, Ahmedabad, India</span>
+          <span className="secondaryText">146, FL 5467, Ahmedabad, India</span>
           <div className="flexCenter f-menu">
             <span>Property</span>
             <span>Services</span>
